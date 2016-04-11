95be8963d3d3c469757f5fbd328cbce20bdaa5a6
Žilvinas <zilvinas930@gmail.com>
2016-04-11 22:39:32 +0300

    Login page'as

--- labanoro-draugai-ui/src/main/webapp/public/js/app.js
+++ labanoro-draugai-ui/src/main/webapp/public/js/app.js
@@ -1,12 +1,29 @@
 /**
  * Created by Žilvinas on 2016-03-11.
  */
+$.material.init();
 var app = angular.module('labanoroDraugaiApp', ['ngRoute']);
 
-app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/',
-        {
-            controller: 'mainController',
-            templateUrl: 'partials/mainView.html'
-        }).otherwise({redirectTo: '/'});
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/',
+            {
+                controller: 'mainController',
+                templateUrl: 'partials/mainView.html'
+            })
+        .when('/login',
+            {
+                controller: 'loginController',
+                templateUrl: 'partials/loginView.html'
+            })
+        .when('/register',
+            {
+                controller: 'registrationController',
+                templateUrl: 'partials/registrationView.html'
+            })
+        .otherwise({redirectTo: '/'});
+
+    $locationProvider.html5Mode({
+        enabled: true
+    });
 }]);
