5d88f45f8890ddfefa760a21f0065bfcee082bf5
Saladinas <jasmontas.lukas@gmail.com>
2016-04-12 00:23:07 +0300

    Added members, profile and residences view and controllers.

--- labanoro-draugai-ui/src/main/webapp/public/js/app.js
+++ labanoro-draugai-ui/src/main/webapp/public/js/app.js
@@ -2,26 +2,47 @@
  * Created by Žilvinas on 2016-03-11.
  */
 $.material.init();
-var app = angular.module('labanoroDraugaiApp', ['ngRoute']);
+var app = angular.module('labanoroDraugaiApp', ['ngRoute', 'ui.router', 'smart-table']);
 
-app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/',
-            {
-                controller: 'mainController',
-                templateUrl: 'partials/mainView.html'
-            })
-        .when('/login',
-            {
-                controller: 'loginController',
-                templateUrl: 'partials/loginView.html'
-            })
-        .when('/register',
-            {
-                controller: 'registrationController',
-                templateUrl: 'partials/registrationView.html'
-            })
-        .otherwise({redirectTo: '/'});
+app.config(['$routeProvider', '$locationProvider', '$stateProvider', '$urlRouterProvider', function($routeProvider, $locationProvider, $stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/");
+
+
+    $stateProvider
+        .state('main', {
+            url: "/main",
+            controller: 'mainController',
+            templateUrl: 'partials/mainView.html'
+        })
+        .state('login', {
+            url: "/login",
+            controller: 'loginController',
+            templateUrl: 'partials/loginView.html'
+        })
+        .state('register', {
+            url: "/register",
+            controller: 'registrationController',
+            templateUrl: 'partials/registrationView.html'
+        })
+        .state('membersList', {
+            url: "/membersList",
+            controller: 'membersListController',
+            templateUrl: 'partials/membersListView.html'
+        })
+        .state('profile', {
+            url: "/profile",
+            controller: 'profileController',
+            templateUrl: 'partials/profileView.html'
+        })
+        .state('residencesList', {
+            url: "/residencesList",
+            controller: 'residencesListController',
+            templateUrl: 'partials/residencesListView.html'
+        });
+
+
+    $urlRouterProvider.otherwise("/main");
 
     $locationProvider.html5Mode({
         enabled: true
--- labanoro-draugai-ui/src/main/webapp/public/js/controllers/controllers.js
+++ labanoro-draugai-ui/src/main/webapp/public/js/controllers/controllers.js
@@ -1,15 +1,6 @@
-/**
- * Created by Žilvinas on 2016-03-11.
- */
 angular.module('labanoroDraugaiApp').controller('mainController', ['$scope', 'userService', function ($scope, userService) {
     userService.test().success(function (result) {//TODO remove
         $scope.testResult = result.email;
     });
-}]);
-
-angular.module('labanoroDraugaiApp').controller('loginController', ['$scope', function ($scope) {
-}]);
-
-angular.module('labanoroDraugaiApp').controller('registrationController', ['$scope', function ($scope) {
 }]);
 
