18ac6cf6c1f68b0bbebe2ce73b1849fcd6a1827c
Žilvinas <zilvinas930@gmail.com>
2016-04-18 23:18:45 +0300

    merge fix

--- labanoro-draugai-ui/src/main/webapp/public/js/app.js
+++ labanoro-draugai-ui/src/main/webapp/public/js/app.js
@@ -2,47 +2,52 @@
  * Created by Žilvinas on 2016-03-11.
  */
 $.material.init();
-var app = angular.module('labanoroDraugaiApp', ['ui.router', 'smart-table']);
+var app = angular.module('labanoroDraugaiApp', ['ui.router', 'satellizer', 'angular-loading-bar', 'smart-table']);
 
-app.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', '$httpProvider', function($locationProvider, $stateProvider, $urlRouterProvider, $httpProvider) {
+app.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', '$httpProvider', '$authProvider', 'cfpLoadingBarProvider',
+    function ($locationProvider, $stateProvider, $urlRouterProvider, $httpProvider, $authProvider, cfpLoadingBarProvider) {
 
-    $urlRouterProvider.otherwise("/");
+        $urlRouterProvider.otherwise("/");
 
-    $stateProvider
-        .state('main', {
-            url: "/",
-            controller: 'mainController',
-            templateUrl: 'partials/mainView.html'
-        })
-        .state('login', {
-            url: "/login",
-            controller: 'loginController',
-            templateUrl: 'partials/loginView.html'
-        })
-        .state('register', {
-            url: "/register",
-            controller: 'registrationController',
-            templateUrl: 'partials/registrationView.html'
-        })
-        .state('membersList', {
-            url: "/membersList",
-            controller: 'membersListController',
-            templateUrl: 'partials/membersListView.html'
-        })
-        .state('profile', {
-            url: "/profile",
-            controller: 'profileController',
-            templateUrl: 'partials/profileView.html'
-        })
-        .state('residencesList', {
-            url: "/residencesList",
-            controller: 'residencesListController',
-            templateUrl: 'partials/residencesListView.html'
+        $stateProvider
+            .state('main', {
+                url: "/",
+                controller: 'mainController',
+                templateUrl: 'partials/mainView.html'
+            })
+            .state('login', {
+                url: "/login",
+                controller: 'loginController',
+                templateUrl: 'partials/loginView.html'
+            })
+            .state('register', {
+                url: "/register",
+                controller: 'registrationController',
+                templateUrl: 'partials/registrationView.html'
+            })
+            .state('membersList', {
+                url: "/membersList",
+                controller: 'membersListController',
+                templateUrl: 'partials/membersListView.html'
+            })
+            .state('profile', {
+                url: "/profile",
+                controller: 'profileController',
+                templateUrl: 'partials/profileView.html'
+            })
+            .state('residencesList', {
+                url: "/residencesList",
+                controller: 'residencesListController',
+                templateUrl: 'partials/residencesListView.html'
+            });
+
+        $httpProvider.interceptors.push('authHttpRequestInterceptor');
+
+        $locationProvider.html5Mode({
+            enabled: true
         });
 
-    $httpProvider.interceptors.push('authHttpRequestInterceptor');
-
-    $locationProvider.html5Mode({
-        enabled: true
-    });
-}]);
+        cfpLoadingBarProvider.includeSpinner = false;
+        $authProvider.loginUrl = '/rest/login';
+        $authProvider.signupUrl = '/rest/register';
+    }]);
