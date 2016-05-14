f1f5b69ebb294cf1490013347184325cc315a1ed
Sebas Hollow <Sebas.V.Hollow@gmail.com>
2016-05-14 03:10:16 +0300

    Extracted addPointsController

--- labanoro-draugai-ui/src/main/webapp/public/js/controllers/member/profileController.js
+++ labanoro-draugai-ui/src/main/webapp/public/js/controllers/member/profileController.js
@@ -60,13 +60,3 @@ app.controller('profileController', ['$scope', '$stateParams', '$uibModal', 'mem
     }
 }]);
 
-app.controller('addPointsController', ['$scope', '$uibModalInstance', function ($scope, $uibModalInstance) {
-    $scope.ok = function () {
-        $uibModalInstance.close($scope.points);
-    };
-
-    $scope.cancel = function () {
-        $uibModalInstance.dismiss('cancel');
-    };
-}]);
-
