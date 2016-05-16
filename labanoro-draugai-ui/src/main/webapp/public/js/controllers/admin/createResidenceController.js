0dd16b9e39bfdb9c68cafae5153d3549537689bd
Sebas Hollow <Sebas.V.Hollow@gmail.com>
2016-05-16 19:31:47 +0300

    extracted services logic into servicesController

--- labanoro-draugai-ui/src/main/webapp/public/js/controllers/admin/createResidenceController.js
+++ labanoro-draugai-ui/src/main/webapp/public/js/controllers/admin/createResidenceController.js
@@ -1,47 +1,28 @@
-app.controller('createResidenceController', ['$scope', 'residencesService', function ($scope, residencesService) {
+app.controller('createResidenceController', ['$scope', '$state', 'residencesService', function ($scope, $state, residencesService) {
+    $scope.Title = 'Sukurti rezidenciją';
     $scope.residence = {};
     $scope.residence.availability = {};
-    $scope.service = {};
-    $scope.price = 0;
-    $scope.name = '';
-    $scope.services = [];
 
-    $scope.Title = 'Sukurti rezidenciją';
-
-    $scope.saveResidence = function () {
+    $scope.saveResidence = function (services) {
+        $scope.residence.extraServices = services;
         //FixMe: This compensates for the -1 day bug.
-        $scope.residence.availability.dateFrom.setHours(3, 0, 0, 0);
-        $scope.residence.availability.dateTo.setHours(3, 0, 0, 0);
+        if ($scope.residence.availability.dateFrom)
+            $scope.residence.availability.dateFrom.setHours(3, 0, 0, 0);
+        if ($scope.residence.availability.dateTo)
+            $scope.residence.availability.dateTo.setHours(3, 0, 0, 0);
+
         console.log($scope.residence);
 
-        $scope.residence.extraServices = $scope.services;
         residencesService.createResidence($scope.residence)
             .then(function (response) {
-                console.log(response);
                 $state.go('main');
             }).catch(function (response) {
+                console.log(response.data);
                 var errorMessage = "";
                 for (var i = 0; i < response.data.length; i++)
                     errorMessage += response.data[i].message + "\n"
-                console.log(response.data);
                 alert("Error: " + errorMessage);
             })
-    };
-
-    $scope.createService = function () {
-        if (($scope.name !== '') && (angular.isDefined($scope.name))) {
-            if (($scope.price !== '') && (angular.isDefined($scope.price))) {
-                $scope.service.name = $scope.name;
-                $scope.service.price = $scope.price;
-                $scope.services.push($scope.service);
-                $scope.name = '';
-                $scope.price = 0;
-            }
-        }
-    };
-
-    $scope.removeService = function (key) {
-        $scope.services.splice(key, 1);
     };
 }]);
 
--- labanoro-draugai-ui/src/main/webapp/public/js/controllers/admin/editResidenceController.js
+++ labanoro-draugai-ui/src/main/webapp/public/js/controllers/admin/editResidenceController.js
@@ -1,19 +1,13 @@
-app.controller('editResidenceController', ['$scope', '$state', 'residencesService', 'residence', function ($scope, $state, residencesService, residence) {
+app.controller('editResidenceController', ['$scope', '$rootScope', '$state', '$timeout', 'residencesService', 'residence', function ($scope, $rootScope, $state, $timeout, residencesService, residence) {
     $scope.residence = residence;
     $scope.residence.availability.dateFrom = new Date(residence.availability.dateFrom);
     $scope.residence.availability.dateTo = new Date(residence.availability.dateTo);
-    console.log($scope.residence);
 
     $scope.Title = "Redaguoti rezidenciją";
-    $scope.service = '';
 
-    if (angular.isDefined($scope.residence.services)) {
-        $scope.services = $scope.residence.services;
-    } else {
-        $scope.services = [];
-    }
-
-    $scope.saveResidence = function () {
+    $scope.saveResidence = function (services) {
+        //FixMe: This compensates for the -1 day bug.
+        $scope.residence.extraServices = services;
         $scope.residence.availability.dateFrom.setHours(3, 0, 0, 0);
         $scope.residence.availability.dateTo.setHours(3, 0, 0, 0);
         console.log($scope.residence);
@@ -22,22 +16,15 @@ app.controller('editResidenceController', ['$scope', '$state', 'residencesServic
             .then(function (response) {
                 $state.go('main');
             }).catch(function (response) {
-            var errorMessage = "";
-            for (var i = 0; i < response.data.length; i++)
-                errorMessage += response.data[i].message + "\n"
-            console.log(response.data);
-            alert("Error: " + errorMessage);
-        })
+                console.log(response.data);
+                var errorMessage = "";
+                for (var i = 0; i < response.data.length; i++)
+                    errorMessage += "\n" + response.data[i].message;
+                alert("Error: " + errorMessage);
+            })
     };
 
-    $scope.createService = function () {
-        if (($scope.service !== '') && (angular.isDefined($scope.service))) {
-            $scope.services.push($scope.service);
-            $scope.service = '';
-        }
-    };
-
-    $scope.removeService = function (key) {
-        $scope.services.splice(key, 1);
-    };
+    $scope.$on('requestServices', function() {
+        $scope.$broadcast('loadServices', residence.extraServices)
+    });
 }]);
