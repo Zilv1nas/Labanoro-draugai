67325fe4c1a60ec09c80129dbb7ebc7dc1fed619
Sebas Hollow <Sebas.V.Hollow@gmail.com>
2016-05-17 17:34:29 +0300

    yay

--- labanoro-draugai-ui/src/main/webapp/public/js/controllers/member/residencesListController.js
+++ labanoro-draugai-ui/src/main/webapp/public/js/controllers/member/residencesListController.js
@@ -1,7 +1,5 @@
 app.controller('residencesListController', ['$scope', '$state', 'residencesService', 'residences', function($scope, $state, residencesService, residences) {
-
     $scope.residences = residences;
-    $scope.onView = function(id){
-        $state.go('viewResidence', {"id":id});
-    }
+
+    //ToDo: Add logic for showing partial list for paging
 }]);
