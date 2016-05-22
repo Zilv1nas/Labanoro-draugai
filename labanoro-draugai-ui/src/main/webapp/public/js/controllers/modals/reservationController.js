a28843b986319fd2ba39ead57a7b55f76e482f58
Jurpys <jurgaitis.mantas@gmail.com>
2016-05-22 23:45:32 +0300

    Added total calculation

--- labanoro-draugai-ui/src/main/webapp/public/js/controllers/modals/reservationController.js
+++ labanoro-draugai-ui/src/main/webapp/public/js/controllers/modals/reservationController.js
@@ -4,6 +4,19 @@ app.controller('reservationController', ['$scope', 'growl', '$filter', '$uibModa
 	$scope.selected = {};
     $scope.selected.services = [];
     $scope.duration = {};
+    $scope.totalPrice = 0;
+
+    $scope.$watch('selected.services', function(o, n){
+    	updateTotalPrice();
+    });
+
+    $scope.$watch('duration.dateFrom', function(o, n){
+    	updateTotalPrice();
+    });
+
+    $scope.$watch('duration.dateTo', function(o, n){
+    	updateTotalPrice();
+    });
 
     $scope.ok = function () {
     	var model = {};
@@ -27,4 +40,36 @@ app.controller('reservationController', ['$scope', 'growl', '$filter', '$uibModa
     $scope.cancel = function () {
         $uibModalInstance.dismiss();
     };
+
+    function calculateWeekCount(dateFrom, dateTo){
+    	if(dateFrom && dateTo ){	    		
+	    	var dF = new Date(dateFrom);
+	    	var dT = new Date(dateTo);
+
+	    	var oneDay = 24*60*60*1000;
+	    	var daysCount = Math.round(Math.abs((dT.getTime() - dF.getTime())/(oneDay)));
+
+	    	if(daysCount < 0){
+	    		return 0;
+	    	}
+
+	    	return parseInt(daysCount / 7) + 1; 
+	    }
+    	return 0;
+    }
+
+    function updateTotalPrice(){
+    	var weekCount = calculateWeekCount($scope.duration.dateFrom, $scope.duration.dateTo);
+    	var servicesPrice = 0;
+    	console.log(weekCount);
+
+    	angular.forEach($scope.selected.services, function(value, key){
+    		servicesPrice += value.price;
+    	});
+
+    	$scope.totalPrice = (servicesPrice + $scope.residence.weeklyPrice) * weekCount;
+
+    	console.log($scope.totalPrice);
+    }
+
 }]);
