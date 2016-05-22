40bc45a93e42c92ed129fa26fb1576ff2d74b8b2
tomaspakala <tomaspakala@gmail.com>
2016-05-22 22:18:25 +0300

    WIP: residence view

--- labanoro-draugai-ui/src/main/webapp/public/js/controllers/member/viewResidenceController.js
+++ labanoro-draugai-ui/src/main/webapp/public/js/controllers/member/viewResidenceController.js
@@ -1,3 +1,13 @@
 app.controller('viewResidenceController', ['$scope', '$state', 'residencesService', 'residence', 'growl', function ($scope, $state, residencesService, residence, growl) {
     $scope.residence = residence;
+
+    $scope.fullPrice = $scope.residence.weeklyPrice;
+
+    $scope.setFullPrice = function(){
+        var price = $scope.residence.weeklyPrice;
+        angular.forEach($scope.residence.extraServices, function(extraService){
+            if (!!extraService.selected) price += extraService.price;
+        })
+        $scope.fullPrice = price;
+    }
 }]);
